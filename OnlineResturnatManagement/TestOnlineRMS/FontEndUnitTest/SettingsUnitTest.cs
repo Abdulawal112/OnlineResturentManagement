758e4d4a99784e634a3522310481d1d2387fa16c
khorshedHasanTutul <khasan7890@gmail.com>
2022-08-10 18:50:28 +0600

    testing SettingsPage but failing

--- OnlineResturnatManagement/TestOnlineRMS/FontEndUnitTest/SettingsUnitTest.cs
+++ OnlineResturnatManagement/TestOnlineRMS/FontEndUnitTest/SettingsUnitTest.cs
@@ -1,14 +1,39 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using OnlineResturnatManagement.Client.Pages;
+using OnlineResturnatManagement.Client.Pages.Setting;
+using OnlineResturnatManagement.Client.Services.IService;
+using OnlineResturnatManagement.Client.Services.Service;
+using OnlineResturnatManagement.Shared;
+using RichardSzalay.MockHttp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace TestOnlineRMS.FontEndUnitTest
 {
     public class SettingsUnitTest
     {
-        
+        [Fact]
+        public void findDefaultWithoutStockSaleIsOffOrNot()
+        {
+            var content = JsonSerializer.Serialize(new List<string> { "data" });
+            var mockHttp = new MockHttpMessageHandler();
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:7095");
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton(httpClient);
+            mockHttp.When("/setting/software-setting")
+                    .Respond(HttpStatusCode.OK, "application/json", content);
 
+            var component = ctx.RenderComponent<SoftwareSetting>();
+            component.WaitForAssertion(() => Assert.NotNull(component.Instance));
+        }
     }
 }
