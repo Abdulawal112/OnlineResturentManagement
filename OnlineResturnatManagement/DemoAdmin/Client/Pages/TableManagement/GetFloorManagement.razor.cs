a5c10b173aa6f2c3f060b5fb07bc0bc1c89a664e
khorshedHasanTutul <khasan7890@gmail.com>
2022-09-18 18:52:02 +0600

    button added table management

---
+++ OnlineResturnatManagement/DemoAdmin/Client/Pages/TableManagement/GetFloorManagement.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace OnlineResturnatManagement.Client.Pages.TableManagement
+{
+    public partial class GetFloorManagement
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await JSRuntime.InvokeAsync<IJSObjectReference>("import", "/monster-admin/js/TableManagement.js");
+            await JSRuntime.InvokeAsync<IJSObjectReference>("import", "/monster-admin/js/DesignedTableManagement.js");
+
+            StateHasChanged();
+            //
+
+        }
+    }
+}
