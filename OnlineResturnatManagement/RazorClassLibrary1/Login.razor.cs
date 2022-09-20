c6283bb3be9b632890f6e4085f841d171f30e020
khorshedHasanTutul <khasan7890@gmail.com>
2022-09-20 16:35:45 +0600

    table management

---
+++ OnlineResturnatManagement/RazorClassLibrary1/Login.razor.cs
@@ -0,0 +1,12 @@
+﻿using OnlineResturnatManagement.Client.HttpRepository;
+using OnlineResturnatManagement.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace OnlineResturnatManagement.Client.Pages
+{
+    public partial class Login
+    {
+        
+    }
+}
